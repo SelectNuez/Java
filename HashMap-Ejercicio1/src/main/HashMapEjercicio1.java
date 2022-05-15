d5a02fd55c3ac54ea0120eba1d7c621a6a3c7e94
LoadingPixelatedTurtle <loadingpixelatedturtle@gmail.com>
2022-05-15 19:14:25 +0200

    Cambio gestión Excepciones

--- HashMap-Ejercicio1/src/main/HashMapEjercicio1.java
+++ HashMap-Ejercicio1/src/main/HashMapEjercicio1.java
@@ -4,7 +4,6 @@
  */
 package main;
 
-import java.lang.*;
 import javax.swing.JOptionPane;
 import index.*;
 
@@ -61,23 +60,17 @@ public class HashMapEjercicio1 {
                         break;
                     case 6:
                         JOptionPane.showMessageDialog(null, "Hasta la proxima!");
-
                         break;
                     default:
                         JOptionPane.showMessageDialog(null, "Introduzca una opcion valida");
                 }
 
+            } catch (java.util.InputMismatchException ex) {
+                JOptionPane.showMessageDialog(null, "Ha surgido un error, por favor, asegurese de introducir correctamente los valores");
+
             } catch (Exception e) {
-                String fallo = e.toString();
-                System.out.println(fallo);
-                if (fallo.contains("java.lang.NumberFormatException: For input string:")) {
-                    JOptionPane.showMessageDialog(null, "Ha introducido una cadena de caracteres no valida");
-
-                } else {
-                    JOptionPane.showMessageDialog(null, "Ha surgido un error, por favor, asegurese de introducir correctamente los valores");
-                }
+                JOptionPane.showMessageDialog(null, "Ha introducido una cadena de caracteres no valida");
             }
-
         } while (menu != 6);
 
     }
